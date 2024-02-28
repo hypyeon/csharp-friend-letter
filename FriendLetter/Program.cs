17d8e208d2cc2e26a01252538831c9b176cb0e50
Hayeong Pyeon <hayeong.pyeon@gmail.com>
2024-02-27 23:17:39 -0800

    add basic web apps in-class practice - controllers, routes, URLs

---
+++ FriendLetter/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FriendLetter
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+      // beginning the process of creating a web app host, by creating a `builder` obj to configure web app host to be built. 
+
+      builder.Services.AddControllersWithViews();
+      // customizing the host `builder` by enabling the MVC framework as a service. invoking the `AddControllerWithVies()` method. 
+      // services get added to web app host thru dependency injection - dependency: a class used within another class. 
+
+      WebApplication app = builder.Build();
+      // creates and returns web app host, saving in the variable `app`. 
+
+      app.UseRouting();
+      // ensuring host to match the website URL - routes to be defined in controller files
+
+      app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}"
+      );
+      // establishing convention for routes to follow
+
+      app.Run();
+      // `app`: middleware - recieves and handles requests from a user to access a page within a website. 
+    }
+  }
+}
